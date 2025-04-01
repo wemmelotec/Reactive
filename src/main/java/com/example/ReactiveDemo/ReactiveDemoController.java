57cad46fc7e139ef55a98601c4ddd46e5f932153
wellington <wellington.melo@sefaz365.pb.gov.br>
2025-03-31 21:25:33 -0300

    second commit

---
+++ src/main/java/com/example/ReactiveDemo/ReactiveDemoController.java
@@ -0,0 +1,25 @@
+package com.example.ReactiveDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ReactiveDemoController {
+
+    @GetMapping("/demo")
+    public Flux<String> getItems() {
+        return getItemsFromDatasource();
+    }
+
+    private Flux<String> getItemsFromDatasource() {
+        return Flux.just("Pen", "Pencil", "Eraser","Notepad"); //Publisher
+    }
+
+    public static void main(String[] args){
+        new ReactiveDemoController().getItems()
+                .log()
+                .subscribe(item->System.out.println("Received: " + item));
+    }
+
+}
---
+++ src/test/java/com/example/ReactiveDemo/ReactiveDemoControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
